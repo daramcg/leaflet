bafd3048885a4a17e730bf7a4b80eb291c6098b4
daramcg <daramcg96@gmail.com>
2022-02-08 21:40:30 +0000

    commit 2

--- js/map.js
+++ js/map.js
@@ -1,9 +1,46 @@
+// globals
+let myMarkers = L.featureGroup();
+let data = [
+	
+    {
+		'title':'Dublin',
+		'lat': 53.34724140858375,
+		'lon':  -6.25918589967779
+	},
+	{
+		'title':'Cork',
+		'lat': 51.8985492413488,
+		'lon': -8.475112410325512
+	}
+]
+// loop through data
+data.forEach(function(item){
+	// create marker
+	let marker = L.marker([item.lat,item.lon]).bindPopup(item.title)
+
+	// add marker to featuregroup
+	myMarkers.addLayer(marker)
+
+})
+// after loop, add the FeatureGroup to map
+myMarkers.addTo(map)
+// zoom to the extent of all markers
+map.fitBounds(myMarkers.getBounds())
+
+
 var map = L.map('map').setView([34.0697,-118.4432], 17);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var marker = L.marker([53.67265486606466, -6.97128513028107]).addTo(map)
-		.bindPopup('Home <br> Where I live')
-		.openPopup();	
+// loop through data
+data.forEach(function(item){
+	// add marker to map
+	var marker = L.marker([item.lat,item.lon]).addTo(map)
+    .bindPopup(item.title)
+    .openPopup();
+})
+
+        // zoom to the extent of all markers
+map.fitBounds(myMarkers.getBounds())
